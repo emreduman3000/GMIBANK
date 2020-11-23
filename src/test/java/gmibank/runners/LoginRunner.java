08db03f7de900afe47b4302ea4b9ec88e8111b32
SelamiUnsal <selamiunsal@gmail.com>
2020-11-23 09:49:01 -0500

    first Api

--- src/test/java/gmibank/runners/LoginRunner.java
+++ src/test/java/gmibank/runners/LoginRunner.java
@@ -5,17 +5,13 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        plugin = {"html:target/default-cucumber-reports",
+@CucumberOptions(plugin = {"html:target/default-cucumber-reports",
                 "json:target/json-reports/cucumber.json",
                 "junit:target/xml-report/cucumber.xml"
         },
         features = "src/test/resources/features",
         glue="gmibank.stepdefinitions",
         tags ="@Negative1",
-        dryRun=false
-
-
-)
+        dryRun=false)
 public class LoginRunner {
 }
--- src/test/java/gmibank/runners/Runner.java
+++ src/test/java/gmibank/runners/Runner.java
@@ -5,14 +5,13 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        plugin = {"html:target/default-cucumber-reports",
+@CucumberOptions(plugin = {"html:target/default-cucumber-reports",
                 "json:target/json-reports/cucumber.json",
                 "junit:target/xml-report/cucumber.xml"
         },
         features = "src/test/resources",
         glue="gmibank/stepdefinitions",
-        tags ="  @customerDataTest",
+        tags ="@customerDataTest",
         dryRun=false
 
 )
