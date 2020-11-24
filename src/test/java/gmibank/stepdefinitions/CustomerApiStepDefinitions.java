6e18f44aa5d8d6c2c5dd16fdf6c4131b0af106bc
betul unsal <btulunsal@gmail.com>
2020-11-24 12:53:51 -0500

    last update

--- src/test/java/gmibank/stepdefinitions/CustomerApiStepDefinitions.java
+++ src/test/java/gmibank/stepdefinitions/CustomerApiStepDefinitions.java
@@ -43,44 +43,44 @@ public class CustomerApiStepDefinitions {
         response.prettyPrint();
     }
 
-    @Given("manipulate all customers data")
-    public void manipulate_all_customers_data() throws Exception {
-        System.out.println("Here is Step2");
+        @Given("manipulate all customers data")
+        public void manipulate_all_customers_data() throws Exception {
+            System.out.println("Here is Step2");
 
-        ObjectMapper obj = new ObjectMapper();
-        customers = obj.readValue(response.asString(), Customer[].class);
-        for (int i = 0; i < customers.length; i++)
-            if (customers[i].getUser() != null)
-                System.out.println(customers[i].getUser().getFirstName());
-
-    }
-
-    @Given("user sets the data in correspondent files")
-    public void user_sets_the_data_in_correspondent_files() {
-        //  WriteToTxt.saveDataInFileWithAllCustomerInfo("AllCustomerData.txt",customers);
-        WriteToTxt.saveDataInFile("NewFile.txt", customers);
-
-    }
-
-    @Then("user validates all data")
-    public void user_validates_all_data() {
-
-        List<Customer> list = ReadTxt.returnCustomerSNN("NewFile.txt");
-        Customer customer = new Customer();
-        customer.setSsn("123-47-2476");
-        int count = 0;
-
-        String expected = "123-47-2476";
-        String expected2 = "098-68-1985";
-        String expected3 = "777-77-7777";
-
-        int expectedNumberOfSSN = 0;
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).getSsn().equals(expected)) {
-                System.out.println("Found item");
-            }
-
+            ObjectMapper obj = new ObjectMapper();
+            customers = obj.readValue(response.asString(), Customer[].class);
+            for (int i = 0; i < customers.length; i++)
+                if (customers[i].getUser() != null)
+                    System.out.println(customers[i].getUser().getFirstName());
 
         }
+
+        @Given("user sets the data in correspondent files")
+        public void user_sets_the_data_in_correspondent_files() {
+            //  WriteToTxt.saveDataInFileWithAllCustomerInfo("AllCustomerData.txt",customers);
+            WriteToTxt.saveDataInFile("NewFile.txt", customers);
+
+        }
+
+        @Then("user validates all data")
+        public void user_validates_all_data() {
+
+            List<Customer> list = ReadTxt.returnCustomerSNN("NewFile.txt");
+            Customer customer = new Customer();
+            customer.setSsn("123-47-2476");
+            int count = 0;
+
+            String expected = "123-47-2476";
+            String expected2 = "098-68-1985";
+            String expected3 = "777-77-7777";
+
+            int expectedNumberOfSSN = 0;
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i).getSsn().equals(expected)) {
+                    System.out.println("Found item");
+                }
+
+
+            }
+        }
     }
-}
