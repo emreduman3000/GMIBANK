6bbc72a5297116d56bf9b97b3fcdd229b2ebbb3e
Yesim Karagulmez <ykara0623@gmail.com>
2020-11-29 01:00:08 +0200

    last

--- src/test/java/gmibank/jsonModels/StateJson.java
+++ src/test/java/gmibank/jsonModels/StateJson.java
@@ -2,6 +2,6 @@ package gmibank.jsonModels;
 
 public class StateJson {
 
-    final public static String CREATE_STATE ="{\"name\": \"LAMUISLAND\"}";
+    final public static String CREATE_STATE4 ="{\"name\": \"LAMUISLAND\"}";
 
 }
