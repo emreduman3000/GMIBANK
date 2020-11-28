337019c823e641c5811f9b07515f81eb1a7aa352
betulunsal <62161853+betulunsal@users.noreply.github.com>
2020-11-28 17:31:53 -0500

    Update StateJson.java

--- src/test/java/gmibank/jsonModels/StateJson.java
+++ src/test/java/gmibank/jsonModels/StateJson.java
@@ -3,5 +3,8 @@ package gmibank.jsonModels;
 public class StateJson {
 
     final public static String CREATE_STATE ="{\"name\": \"LAMUISLAND\"}";
+    final public static String CREATE_STATE1 = "{\"countryId\": 1,\"name\": \"GEORGIA\"}";
+    final public static String CREATE_STATE2 = "{\"countryId\": 1,\"name\": \"UTAH\"}";
+    final public static String CREATE_STATE3 = "{\"countryId\": 1,\"name\": \"NEWYORK\"}";
 
 }
